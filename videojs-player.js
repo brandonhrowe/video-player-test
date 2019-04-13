ee7ad574b2fdfb7f74feeb38d3f54877115e0850
Brandon Rowe <brandonhrowe@gmail.com>
2019-04-12 20:17:05 -0400

    videoJS implementation started

---
+++ videojs-player.js
@@ -0,0 +1,9 @@
+const video = videojs("vid-js-test", {
+  controls: true,
+  autoplay: false,
+  loop: true,
+  preload: "metadata",
+  width: "720px",
+  height: "480px",
+  poster: "detour-image.png"
+});
